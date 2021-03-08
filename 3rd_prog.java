03fd7ac299223f62b1d462518f1c5e251f0e2513
Tanishq-Yadav981 <73771533+Tanishq-Yadav981@users.noreply.github.com>
2021-03-08 16:17:14 +0530

    Update 3rd_prog.java

--- 3rd_prog.java
+++ 3rd_prog.java
@@ -6,18 +6,18 @@ import java.util.*;
 class DisplayOperations extends Operations
 {
     public void showAttributes() //method to display all attributes
-	{
-        System.out.prinltn(this.id); //displays id
-        System.out.prinltn(this.name); //displays name
-        System.out.prinltn(this.percentage); // displays percentage
-        System.out.prinltn(this.skills); // displays skills
+    {
+	System.out.prinltn(this.id); //displays id
+	System.out.prinltn(this.name); //displays name
+	System.out.prinltn(this.percentage); // displays percentage
+	System.out.prinltn(this.skills); // displays skills
     }
     public void showNAME()  // method to show name in UPPERCASE
-	{
+    {
         System.out.prinltn(this.name.toUpperCase()); //prints name in UPPERCASE
     }
     public void showDifference(Student s2) // method to compare percentage of two different students
-	{
+    {
         System.out.prinltn(this.percentage-s2.percentage); //prints difference of percentage of two students
     }
 }
